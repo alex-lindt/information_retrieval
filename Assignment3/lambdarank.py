f7353473b77784554cb654284b387740dce00146
David Biertimpel <david.biertimpel@protonmail.com>
2020-03-17 09:49:12 +0100

    Implemented speedup RankNet (82 ndcg)

--- Assignment3/lambdarank.py
+++ Assignment3/lambdarank.py
@@ -17,6 +17,8 @@ from pointwise_ltr import (
     progress_over_last
 )
 
+import pandas as pd
+
 
 class LambdaRank(nn.Module):
     def __init__(self, n_hidden):
@@ -48,6 +50,7 @@ def train_lambda_rank(ARGS, data, model):
 
     queries = np.arange(0, data.train.num_queries())
 
+    print(f"Starting {ARGS.epochs} epochs: ")
     for epoch in range(ARGS.epochs):
 
         loss_epoch = []
@@ -57,26 +60,24 @@ def train_lambda_rank(ARGS, data, model):
 
             scores = model(X)
 
-            loss = lambda_rank_loss(scores, y)
-
-            print((loss / y.shape[0]).sum())
+            with torch.no_grad():
+                loss = lambda_rank_loss(scores, y, ARGS.irm_type)
 
             # loss_epoch.append(loss.item())
 
             # optimize
             optimizer.zero_grad()
-            scores.backward(loss[:, None] / y.shape[0])
+            scores.backward(loss[:, None])
             optimizer.step()
 
         loss_curve.append(loss_epoch)
 
         # compute NDCG on validation set
-        # val_mean, _ = evaluate_model(model, data.validation, device)
-        # ndcg_val_curve.append(val_mean)
-        #
-        # print(f"[Epoch {epoch}] loss: {loss.item()} validation ndcg: ({val_mean})")
-        # epoch += 1
-        #
+        val_mean, _ = evaluate_model(model, data.validation, ARGS.device)
+        ndcg_val_curve.append(val_mean)
+
+        print(f"[Epoch {epoch}] loss: No idea validation ndcg: ({val_mean})")
+
         # early stopping using NDCG on validation set
         # if not progress_over_last(ndcg_val_curve):
         #     break
@@ -84,45 +85,52 @@ def train_lambda_rank(ARGS, data, model):
     return model, loss_curve, ndcg_val_curve
 
 
-def create_S(y):
-    S = np.zeros((y.shape[0], y.shape[0]))
-    for i, eli in enumerate(y):
-        for j, elj in enumerate(y):
-            if eli > elj:
-                S[i, j] = 1
-            if eli < elj:
-                S[i, j] = -1
-            else:
-                S[i, j] = 0
-    return torch.FloatTensor(S)
-
-
-def create_score_diffs(scores, gamma):
-    return 1 / (1.0 + torch.exp(-gamma * (scores - scores.t())))
-
-    # Sc = torch.zeros(scores.shape[0], scores.shape[0])
-    # for i, si in enumerate(scores):
-    #     for j, sj in enumerate(scores):
-    #         Sc[i, j] = -gamma * (si - sj)
-    # return 1 / (1 + torch.exp(Sc))
+def lambda_rank_loss(scores, y, irm_type, gamma=1.0):
+    return rank_net_loss(scores, y, gamma) * irm_delta(scores, y[:, None], irm_type)
 
 
 def rank_net_loss(scores, y, gamma):
-    S = create_S(y)
-    Sc = create_score_diffs(scores, gamma)
+    Sc, S = create_matrices(scores, gamma, y)
 
     _lambda = gamma * (0.5 * (1 - S) - Sc)
 
     return _lambda.sum(dim=1)
 
 
-def irm_delta(irm_loss):
-    pass
+def irm_delta(scores, y, irm_type):
+    if irm_type == "ndcg":
+        y_rels = torch.pow(2.0, y) - 1
+        acc_gain = y_rels - y_rels.t()
+
+        _idxs = y.argsort(dim=0, descending=True)
+
+#         TODO. Finish up
 
 
-def lambda_rank_loss(scores, y, gamma=1, irm_type="ndcg"):
-    # return rank_net_loss(scores, y) * irm_delta(irm_type)
-    return rank_net_loss(scores, y, gamma)
+
+def create_matrices(scores, gamma, y):
+    Sc = 1 / (1.0 + torch.exp(gamma * (scores - scores.t())))
+    # Sc = torch.zeros(scores.shape[0], scores.shape[0])
+    # for i, si in enumerate(scores):
+    #     for j, sj in enumerate(scores):
+    #         Sc[i, j] = gamma * (si - sj)
+    # return 1 / (1 + torch.exp(Sc))
+
+    # S = torch.zeros_like(Sc)
+    # S[Sc > 0] = 1
+    # S[Sc == 0] = 0
+    # S[Sc < 0] = -1
+
+    S = torch.zeros_like(Sc)
+    for i, eli in enumerate(y):
+        for j, elj in enumerate(y):
+            if eli > elj:
+                S[i, j] = 1
+            elif eli < elj:
+                S[i, j] = -1
+            else:
+                S[i, j] = 0
+    return Sc, torch.FloatTensor(S)
 
 
 def sample_batch(data_split, queries, device):
@@ -147,9 +155,10 @@ if __name__ == "__main__":
 
     # Training
     parser.add_argument('--epochs', type=int, default=500, help='number of epochs')
-    parser.add_argument('--n-hidden', type=int, default=100, help='number of hidden layer')
-    parser.add_argument('--bpe', type=int, default=100, help='Batches per epoch')
-    parser.add_argument('--lr', type=float, default=0.003, help='learning rate')
+    parser.add_argument('--n-hidden', type=int, default=256, help='number of hidden layer')
+    parser.add_argument('--bpe', type=int, default=10, help='Batches per epoch')
+    parser.add_argument('--lr', type=float, default=0.0001, help='learning rate')
+    parser.add_argument('--irm-type', type=str, default="ndcg", help="Training device 'cpu' or 'cuda:0'")
     parser.add_argument('--device', type=str, default="cpu", help="Training device 'cpu' or 'cuda:0'")
 
     ARGS = parser.parse_args()
