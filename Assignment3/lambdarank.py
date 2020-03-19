d66e2f28f2ca9a7eb745b02aed1b23a72c0ca65c
David Biertimpel <david.biertimpel@protonmail.com>
2020-03-19 21:54:50 +0100

    Implemented ERR for evaluation

--- Assignment3/lambdarank.py
+++ Assignment3/lambdarank.py
@@ -12,11 +12,6 @@ import dataset
 import ranking as rnk
 import evaluate as evl
 
-from pointwise_ltr import (
-    evaluate_model,
-    progress_over_last
-)
-
 import pandas as pd
 
 
@@ -65,11 +60,13 @@ def train_lambda_rank(ARGS, data, model):
 
             lambdas = lambda_rank_loss(scores, y, ARGS.irm_type)
 
-            loss = (scores * lambdas.detach()).sum()
+            loss = (scores * lambdas.detach()).mean()
+            loss_epoch.append(loss.item())
 
             # optimize
             optimizer.zero_grad()
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 10)
             optimizer.step()
 
             scheduler.step()
@@ -77,7 +74,7 @@ def train_lambda_rank(ARGS, data, model):
         loss_curve.append(loss_epoch)
 
         # compute NDCG on validation set
-        val_mean, _ = evaluate_model(model, data.validation, ARGS.device)
+        val_mean, _ = calculate_ndcg(model, data.validation, ARGS.device)
         ndcg_val_curve.append(val_mean)
 
         print(f"[Epoch {epoch}] val ndcg: {np.round(val_mean, 4)}, lr: {scheduler.get_lr()}")
@@ -90,30 +87,30 @@ def train_lambda_rank(ARGS, data, model):
 
 
 def lambda_rank_loss(scores, y, irm_type, gamma=1.0):
-    _lamb = rank_net_loss(scores, y, gamma)
-    _irm = irm_delta(scores, y, irm_type)
-    lamb_irm = _lamb * _irm
-    return lamb_irm.sum(dim=1)[:, None]
-
-
-def rank_net_loss(scores, y, gamma):
     Sc, S = create_matrices(scores, gamma, y)
     _lambda = gamma * (0.5 * (1 - S) - Sc)
-    return _lambda
+    _irm = irm_delta(scores, y, irm_type)
+    lamb_irm = _lambda * _irm
+    return lamb_irm.mean(dim=1)[:, None]
 
 
 def irm_delta(scores, y, irm_type):
     scores = scores.detach().numpy().flatten()
     labels = y.detach().numpy().flatten()
 
+    random_i = np.random.permutation(
+        np.arange(scores.shape[0])
+    )
+    labels = labels[random_i]
+    scores = scores[random_i]
+
+    sort_ind = np.argsort(scores)[::-1]
+    sorted_labels = labels[sort_ind] + 1
+
     if irm_type == "ndcg":
+        ideal_labels = np.sort(labels)[::-1] + 1
 
         k = scores.shape[0]
-
-        sort_ind = np.argsort(scores)[::-1]
-
-        sorted_labels = labels[sort_ind] + 1
-        ideal_labels = np.sort(labels)[::-1] + 1
 
         _dcg = evl.dcg_at_k(sorted_labels, k)
         idcg = evl.dcg_at_k(ideal_labels, k)
@@ -125,6 +122,7 @@ def irm_delta(scores, y, irm_type):
                 _scores = np.copy(scores)
                 _labels = np.copy(labels)
                 _scores[i], _scores[j] = _scores[j], _scores[i]
+                # TODO: also swap labels?
                 # _labels[i], _labels[j] = _labels[j], _labels[i]
 
                 # Taken from evaluation function
@@ -138,31 +136,16 @@ def irm_delta(scores, y, irm_type):
 
     elif irm_type == "err":
 
-        grades = (np.power(2.0, labels) - 1) / 16
-
-        err = []
-        past_p = []
-        for k in range(labels.shape[0]):
-            err_i = (1 / (k + 1)) * grades[k] * np.prod(past_p)
-            err.append(err_i)
-            past_p.append(1 - grades[k])
-        ref_err = np.sum(err)
+        grades = (np.power(2.0, sorted_labels) - 1) / 16
+        ref_err = calc_err(grades)
 
         _errs = np.zeros((labels.shape[0], labels.shape[0]))
         for i in range(labels.shape[0]):
             for j in range(labels.shape[0]):
-
                 _grades = np.copy(grades)
                 _grades[i], _grades[j] = _grades[j], _grades[i]
 
-                err = []
-                past_p = []
-                for k in range(labels.shape[0]):
-                    err_i = (1 / (k + 1)) * _grades[k] * np.prod(past_p)
-                    err.append(err_i)
-                    past_p.append(1 - _grades[k])
-
-                _errs[i, j] = np.abs(np.sum(err) - ref_err)
+                _errs[i, j] = np.abs(calc_err(_grades) - ref_err)
 
         return torch.FloatTensor(_errs)
 
@@ -193,6 +176,75 @@ def create_matrices(scores, gamma, y):
     return Sc, torch.FloatTensor(S)
 
 
+def calculate_ndcg(model, data_split, device, print_results=False):
+    """
+    Function calculate ndcg
+    """
+    scores = model.forward(torch.Tensor(data_split.feature_matrix).to(device))
+    scores = scores.cpu().detach().numpy().reshape(-1)
+    return evl.evaluate(data_split, scores, print_results)["ndcg"]
+
+
+def calculate_err(model, data_split, device, print_results=False):
+    results = {}
+    for qid in np.arange(data_split.num_queries()):
+        if evl.included(qid, data_split):
+            qd_features = data_split.query_feat(qid)
+            scores = model(torch.Tensor(qd_features).to(device))
+            labels = data_split.query_labels(qid)
+
+            scores = scores.detach().numpy().flatten()
+            labels = labels.flatten()
+
+            random_i = np.random.permutation(
+                np.arange(scores.shape[0])
+            )
+            labels = labels[random_i]
+            scores = scores[random_i]
+
+            sort_ind = np.argsort(scores)[::-1]
+            sorted_labels = labels[sort_ind] + 1
+            grades = (np.power(2.0, sorted_labels) - 1) / 16
+
+            current_results = {
+                'err': calc_err(grades, 0),
+                'err@03': calc_err(grades, 3),
+                'err@05': calc_err(grades, 5),
+                'err@10': calc_err(grades, 10),
+                'err@20': calc_err(grades, 20),
+            }
+
+            evl.add_to_results(results, current_results)
+
+    print('"metric": "mean" ("standard deviation")')
+    mean_results = {}
+    for k in sorted(results.keys()):
+        v = results[k]
+        mean_v = np.mean(v)
+        std_v = np.std(v)
+        mean_results[k] = (mean_v, std_v)
+        if print_results:
+            print('%s: %0.04f (%0.05f)' % (k, mean_v, std_v))
+    return mean_results["err"]
+
+
+def calc_err(grades, k=0):
+    if k > 0:
+        k = min(grades.shape[0], k)
+    else:
+        k = grades.shape[0]
+    grades = grades[: k]
+
+    err = []
+    past_p = []
+    for k in range(grades.shape[0]):
+        err_i = (1 / (k + 1)) * grades[k] * np.prod(past_p)
+        err.append(err_i)
+        past_p.append(1 - grades[k])
+
+    return np.sum(err)
+
+
 def sample_batch(data_split, queries, device):
     """
     Randomly sample batch from data_split.
@@ -215,11 +267,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Training
-    parser.add_argument('--epochs', type=int, default=500, help='number of epochs')
+    parser.add_argument('--epochs', type=int, default=5, help='number of epochs')
     parser.add_argument('--n-hidden', type=int, default=256, help='number of hidden layer')
     parser.add_argument('--bpe', type=int, default=10, help='Batches per epoch')
     parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
-    parser.add_argument('--irm-type', type=str, default="err", help="Training device 'cpu' or 'cuda:0'")
+    parser.add_argument('--irm-type', type=str, default="ndcg", help="Training device 'cpu' or 'cuda:0'")
     parser.add_argument('--device', type=str, default="cpu", help="Training device 'cpu' or 'cuda:0'")
 
     ARGS = parser.parse_args()
@@ -232,4 +284,10 @@ if __name__ == "__main__":
 
     model = LambdaRank(ARGS.n_hidden)
 
-    train_lambda_rank(ARGS, data, model)
+    model, loss_curve, ndcg_val_curve = train_lambda_rank(ARGS, data, model)
+
+    mean_results_ndcg = calculate_ndcg(model, data.test, ARGS.device, True)
+    mean_results_err = calculate_err(model, data.test, ARGS.device, True)
+
+    print(f"Mean nDCG:{mean_results_ndcg}")
+    print(f"Mean ERR:{mean_results_err}")
