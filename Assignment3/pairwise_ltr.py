a2aa7b4626e72102e599ee5929fadfbbcab9182e
VanessaBotha <vanessabotha96@gmail.com>
2020-03-21 15:45:00 +0100

    right loss plot for Ranknet sped up

--- Assignment3/pairwise_ltr.py
+++ Assignment3/pairwise_ltr.py
@@ -1,6 +1,7 @@
 import dataset
 import torch
 from torch import nn
+import time
 import numpy as np
 from itertools import permutations 
 import matplotlib.pyplot as plt
@@ -41,14 +42,20 @@ class RankNet(nn.Module):
         # Assignment Equation (3)
         C = 0.5 * (1 - S) * self.gamma * score_diff + torch.log2(1 + torch.exp(-self.gamma * score_diff))
 
+    
         # # WITH SAMPLING FIXED BATCH SIZE
         # B = np.ones(C.shape) - np.eye(C.shape[0])
         # idx = B.nonzero()
-        # i = np.arange(len(idx[0]))
-        # np.random.shuffle(i)
-        # sample = i[:self.batch_size]
+        # sample = [np.random.randint(0, len(idx[0])) for i in range(self.batch_size)]
+
+        # # i = np.arange(len(idx[0]))
+        # # np.random.shuffle(i)
+        # # sample = i[:self.batch_size]
         # idx1, idx2 = idx[0][sample], idx[1][sample]
+    
         # C_T = C[idx1, idx2].sum()
+
+        # return C_T
 
         #WITH MEAN 
         # pairs on the diagonal are not valid
@@ -58,10 +65,16 @@ class RankNet(nn.Module):
         return C_mean
 
     def spedup_loss(self, scores, labels, ):
-        pass
-        # Sc, S = create_matrices(scores, self.gamma, labels)
-        # _lambda = gamma * (0.5 * (1 - S) - Sc)
-        # return _lambda.sum(dim=1)[:, None]
+        Sc, S = create_matrices(scores, self.gamma, labels)
+        _lambda = self.gamma * (0.5 * (1 - S) - Sc)
+
+        # set diagonal of lambda to zero 
+        lambda_0 = _lambda  * (torch.ones_like(_lambda, dtype=torch.float) - torch.eye(_lambda.shape[0], dtype=torch.float)) 
+
+        # average across i 
+        lambdas = (lambda_0.sum(dim=1) / (lambda_0.shape[1] - 1))[:, None]
+
+        return (scores * lambdas.detach()).mean()
 
             
 
@@ -73,7 +86,7 @@ def get_samples_by_qid(qid, data_split, device):
     return torch.Tensor(docs).to(device), torch.Tensor(labels).to(device) 
 
 
-def train_ranknet(data, epochs=100, n_hidden=1024, gamma=1.0, lr=1e-4, batch_size=10, spedup = True, device='cpu'):
+def train_ranknet(data, epochs=100, n_hidden=1024, gamma=1.0, lr=1e-4, batch_size=50, spedup = True, device='cpu'):
 
 
     model = RankNet(n_hidden=n_hidden, gamma=gamma, batch_size=batch_size, device=device)
@@ -81,9 +94,9 @@ def train_ranknet(data, epochs=100, n_hidden=1024, gamma=1.0, lr=1e-4, batch_siz
     optimizer = torch.optim.Adam(model.parameters(), lr=lr) 
 
     if spedup:
-        loss_fn = RankNet.spedup_loss
+        loss_fn = model.spedup_loss
     else:
-        loss_fn = RankNet.loss
+        loss_fn = model.loss
 
     # track loss and ndcg on validation set 
     loss_curve = []
@@ -112,8 +125,14 @@ def train_ranknet(data, epochs=100, n_hidden=1024, gamma=1.0, lr=1e-4, batch_siz
 
             scores = model.forward(docs)
 
-            loss = loss_fn(model, scores, labels)
-            loss_epoch.append(loss.item())
+            loss = loss_fn(scores, labels)
+
+            if spedup:
+                loss_report = model.loss(scores, labels).item()
+                loss_epoch.append(loss_report)
+            else:
+                loss_report = loss.item()
+                loss_epoch.append(loss_report)
 
             # optimize
             optimizer.zero_grad()        
@@ -129,7 +148,7 @@ def train_ranknet(data, epochs=100, n_hidden=1024, gamma=1.0, lr=1e-4, batch_siz
         val_mean, _ = evaluate_ranknet(model, data.validation, device)
         ndcg_val_curve.append(val_mean)
 
-        print(f"[Epoch {epoch}] loss: {loss.item()} validation ndcg: ({val_mean})")
+        print(f"[Epoch {epoch}] loss: {loss_report} validation ndcg: ({val_mean})")
 
         # early stopping using NDCG on validation set
         if not progress_over_last(ndcg_val_curve):
@@ -145,6 +164,34 @@ def evaluate_ranknet(model, data_split, device, metric='ndcg'):
     if metric:
         return evl.evaluate(data_split, scores)[metric]
     return evl.evaluate(data_split, scores)
+
+def plot_pairwise_ltr(model, loss_curve, ndcg_val_curve):
+    x = np.linspace(0, len(ndcg_val_curve) + 1, len(ndcg_val_curve))
+
+    loss_means = np.array([np.mean(l) for l in loss_curve])
+    loss_stds = np.array([np.std(l) for l in loss_curve])
+
+    # plt.title("Loss vs. NDCG on Validation Data")
+    # plt.xlabel("Epochs")
+    # plt.plot(x, loss_means, label='loss over epochs')
+    # plt.fill_between(x, loss_means - loss_stds, loss_means + loss_stds, alpha=0.2)
+    # plt.plot(x, ndcg_val_curve, label='NDCG on validation data')
+    # plt.legend()
+
+    plt.title("NDCG on Validation Data")
+    plt.xlabel("Epochs")
+    plt.plot(x, ndcg_val_curve, label='NDCG on validation data')
+    plt.legend()
+    plt.savefig('pairwise_NDCG')
+
+    plt.clf()
+    plt.title("Loss on Validation Data")
+    plt.xlabel("Epochs")
+    plt.plot(x, loss_means, label='loss over epochs')
+    plt.fill_between(x, loss_means - loss_stds, loss_means + loss_stds, alpha=0.2)
+    plt.legend()
+    plt.savefig('pairwise_loss')
+
 
 
 if __name__ == "__main__":
@@ -163,10 +210,19 @@ if __name__ == "__main__":
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model, loss_curve, ndcg_val_curve = train_ranknet(data, epochs=10, spedup=False, device = device)
+
+    # tic = time.perf_counter()
+    model, loss_curve, ndcg_val_curve = train_ranknet(data, epochs=100, spedup=False, device = device,  lr=1e-5)
+
+    # toc = time.perf_counter() 
+    # print(f"Trained in {toc - tic:0.4f} seconds")
+
+
     test_mean = evaluate_ranknet(model, data.test, device, metric=None)
 
     print(test_mean)
+
+    plot_pairwise_ltr(model, loss_curve, ndcg_val_curve)
 
     
 
